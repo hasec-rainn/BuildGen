e5444cc6065be41058c650c4cc7ea8163113bee4
SkyBoundCaptain <hasecrainn@gmail.com>
2021-05-27 18:07:15 -0700

    Added basic functionality of rptools.js as a server

--- rptools.js
+++ rptools.js
@@ -1,8 +1,22 @@
 
 var express = require("express")
 var app = express()
-var port = PORT
+var port = 3000
 
 app.listen(port, SuccsessfulConnection)
 function SuccsessfulConnection() {
     console.log("\nSuccessfully connected to port ", port) }
+
+app.use(RequestReceived)
+function RequestReceived(req, res, next) {
+    console.log("Request received:")
+    console.log("\turl: ", req.url)
+    console.log("\tmethod: ", req.method)
+    console.log("\theaders: ", req.headers)
+    next()
+}
+
+app.get("/", GiveHome)
+function GiveHome(req, res, next) {
+    next()
+}
