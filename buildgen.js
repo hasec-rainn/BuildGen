281fcc015bfd86de12c6565d2e500b1818c8cbd2
SkyBoundCaptain <hasecrainn@gmail.com>
2021-05-29 11:51:55 -0700

    Added/modified theme files and buildgen

--- buildgen.js
+++ buildgen.js
@@ -1,6 +1,19 @@
-/*Filereader that reads in the descriptions*/
-var textReader = new FileReader()
+var fs = require("fs")
 
+/*Generates a random number from zero up to and including max*/
+    function RandNum(max) {
+        return Math.floor(Math.random() * (max+1)) }
+
+/*All theme files read in. Not yet split up with split.*/
+    var template = fs.readFileSync("./themes/theme_template.txt","utf8")
+
+/*Themes split up string arrays. Each element is its own description*/
+    var templateArr = template.split("\n")
+
+/*Array of string arrays. 2D matrix*/
+    themeArr = {templateArr}
+
+/*The descriptor container*/
 var descriptors = {
     north : "",
     east : "",
@@ -9,3 +22,7 @@ var descriptors = {
     center : "",
     overview : ""
 }
+
+console.log("Output: ", template)
+console.log("Output 2: ", templateArr[0])
+
