cb188259f905392dfbef04aaf955b383eef10409
SkyBoundCaptain <hasecrainn@gmail.com>
2021-05-30 11:37:50 -0700

    Created new themes; added them to buildgen.js

--- buildgen.js
+++ buildgen.js
@@ -4,24 +4,32 @@ var fs = require("fs")
     var spooky = fs.readFileSync("./themes/spooky.txt","utf8")
     var spooky_overview = fs.readFileSync("./themes/spooky_overview.txt","utf8")
     var magicshop = fs.readFileSync("./themes/magicshop.txt","utf8")
-    //var magicshop_overivew = fs.readFileSync("./themes/spooky_overview.txt","utf8")
+    var magicshop_overivew = fs.readFileSync("./themes/magicshop_overview.txt","utf8")
+    var generalshop = fs.readFileSync("./themes/generalshop.txt","utf8")
+    var generalshop_overview = fs.readFileSync("./themes/generalshop_overview.txt","utf8")
 
 /*Themes split up string arrays. Each element is its own description*/
     var spookyArr = spooky.split("\n")
     var spooky_overviewArr = spooky_overview.split("\n")
     var magicshopArr = magicshop.split("\n")
+    var magicshop_overviewArr = magicshop_overivew.split("\n")
+    var generalshopArr = generalshop.split("\n")
+    var generalshop_overviewArr = generalshop_overview.split("\n")
 
 /*Array of string arrays. 2D matrix containing all themes, 
 used to pick a random description from a random theme*/
     var themeArr = [
         spookyArr,
-        magicshopArr
+        magicshopArr,
+        generalshopArr
     ]
 
 /*Array of string arrays. 2D matrix containing all themes' overview, 
 used to pick a random description from a random theme*/
     var theme_overviewArr = [
-        spooky_overviewArr
+        spooky_overviewArr,
+        magicshop_overviewArr,
+        generalshop_overviewArr
     ]
 
 /*The descriptor container*/
@@ -42,12 +50,12 @@ used to pick a random description from a random theme*/
 
 
 /*Generates a random number from zero up to but not including max*/
-function RandNum(max) {
-    return Math.floor(Math.random() * max) }
+    function RandNum(max) {
+        return Math.floor(Math.random() * max) }
 
 /*Generates a random number from 1 up to but not including max*/
-function RandNumMod(max) {
-    return Math.floor((Math.random() * (max-1)) + 1) }
+    function RandNumMod(max) {
+        return Math.floor((Math.random() * (max-1)) + 1) }
 
 
 /*Repeats 5 times to fill in the first 5 descriptors. Could be made more efficient*/
