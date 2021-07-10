ecee66056bc067fc7233bf28141c26c690d0638b
SkyBoundCaptain <hasecrainn@gmail.com>
2021-07-09 19:01:19 -0700

    Updated how to add new themes; added aquatic theme

--- public/buildgen.js
+++ public/buildgen.js
@@ -13,6 +13,8 @@ splitting them up into string arrays.*/
     var goblinCave_overview = fs.readFileSync("./themes/goblinCave_overview.txt", "utf8")
     var wizardTower = fs.readFileSync("./themes/wizardTower.txt", "utf8")
     var wizardTower_overview = fs.readFileSync("./themes/wizardTower_overview.txt", "utf8")
+    var aquatic = fs.readFileSync("./themes/aquatic.txt", "utf8")
+    var aquatic_overview = fs.readFileSync("./themes/aquatic_overview.txt", "utf8")
 
 /*Themes split up into string arrays. Each element is its own description*/
     var spooky = spooky.split("\n")
@@ -25,6 +27,8 @@ splitting them up into string arrays.*/
     var goblinCave_overview = goblinCave_overview.split("\n")
     var wizardTower = wizardTower.split("\n")
     var wizardTower_overview = wizardTower_overview.split("\n")
+    var aquatic = aquatic.split("\n")
+    var aquatic_overview = aquatic_overview.split("\n")
 
 /**************************************End Required Files********************************************************/
 /*The above should only be called once (when the page is initially loaded) */
@@ -36,7 +40,8 @@ var buildGen = {
         "Magic Shop Room": magicshop,
         "General Shop Room": generalshop,
         "Goblin Cave Room": goblinCave,
-        "Wizard Tower Room": wizardTower
+        "Wizard Tower Room": wizardTower,
+        "Aquatic Room": aquatic
     },
 
     overviews: {
@@ -44,7 +49,8 @@ var buildGen = {
         "Magic Shop Room": magicshop_overview,
         "General Shop Room": generalshop_overview,
         "Goblin Cave Room": goblinCave_overview,
-        "Wizard Tower Room": wizardTower_overview
+        "Wizard Tower Room": wizardTower_overview,
+        "Aquatic Room": aquatic_overview
     }
 }
 
